2c4ec1538963814b8c2358d675d5b1ccf06c8052
Ege Yay <egeyay@outlook.com>
2017-10-22 04:20:01 -0400

    Update hw1.ml

--- hw1.ml
+++ hw1.ml
@@ -1,33 +1,5 @@
-(* Student information:
-
-   Enter your name, and if you chose to work in pairs, the name of the
-   student you worked with (both students MUST submit the solution to
-   myCourses):
-
-   Name:        Ege Yay
-   McGill ID:   260679652
-
-   If you worked in pairs, the name of the other student.
-
-   Name:        Halil Murat Goksel
-   McGill ID:   260572202
 
 
- *)
-
-(* Notice: by submitting as part of team, you declare that you worked
-   together on the solution. Submissions in pairs are allowed to
-   foster team work, they have to be developed by both students *)
-
-(* Homework 1 - Questions 2 and 3 *)
-
-(* First, some utility functions and declarations that you can use. Be
-   sure to check Ocaml's documentation to find more functions
-   available to you.
-
-   You can start checking the documentation at:
-   https://caml.inria.fr/pub/docs/manual-ocaml/libref/Pervasives.html
- *)
 
 (* the value of pi *)
 let pi : float =  acos ~-.1.0
@@ -46,7 +18,6 @@ let is_multiple_of (a : float) (b : float) : bool =
 (* a function to check if a is between plus/minus b *)
 let abs_btwn a b = a < b && a > ~-.b
 
-(* Question 2: Triangles are the best *)
 
 type side = float
 
@@ -57,7 +28,7 @@ type tr_kind
   | Equilateral
   | Isosceles
 
-(* Question 2.1 *)
+
 let well_formed_by_sides (a, b, c : tr_by_sides) : bool =
   if positive a && positive b && positive c then
     if a+.b > c && b+.c > a && c+.a > b then true
@@ -65,7 +36,7 @@ let well_formed_by_sides (a, b, c : tr_by_sides) : bool =
   else false
 
 
-(* Question 2.2 *)
+
 let create_triangle (kind : tr_kind) (area : float) : tr_by_sides =
   let e (area:float) = sqrt (2.0 *. area /. sin 1.0472) in
   let iso_side (area:float) = sqrt (4.0 *. area /. sqrt 3.0) in
@@ -99,18 +70,11 @@ let angle_to_sides (a, b, gamma) =
   else
     None
 
-(* Now that you implemented Q2.2 and saw the new representation of
-   triangles by two sides and the angle between them, also ponder on
-   how one represents Q2.2 using this representation. The idea is to
-   think about how the representation helps make illegal states hard
-   to represent and how easy and hard it is to implement the
-   algorithm. *)
 
-(* Question 3: Flexing recursion and lists *)
 
 let even (n : int) : bool = n mod 2 = 0
 
-(* Question 3.1 *)
+
 let evens_first (l : int list) : int list =
   let odd n = not (even n) in
   let evens = List.filter even l in 
@@ -120,7 +84,7 @@ let evens_first (l : int list) : int list =
 let ex_1 = evens_first [7 ; 5 ; 2; 4; 6; 3; 4; 2; 1]
 (* val ex_1 : int list = [2; 4; 6; 4; 2; 7; 5; 3; 1] *)
 
-(* Question 3.2 *)
+
 let even_streak (l : int list) : int =
   let rec aux count max = function
     |[]   -> max
@@ -134,10 +98,8 @@ let even_streak (l : int list) : int =
 
 let ex_2 = even_streak [7; 2; 4; 6; 3; 4; 2; 1]
 
-(* val ex_2 : int = 3 *)
 
-
-(* Question 3.3 *)
+(*  D D D D D D D D D D D N N N N N N N N N N N N N A A A A A A A A A A A    *)
 
 type nucleobase = A | G | C | T
 
